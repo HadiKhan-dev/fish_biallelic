375f3bc466b378ad63c8a3f466ad6f721c13f342
Hadi Khan <101122077+HadiKhan-dev@users.noreply.github.com>
2024-12-16 00:09:05 +0000

    Add simulation code

--- simulate_sequences.py
+++ simulate_sequences.py
@@ -43,7 +43,8 @@ def pairup_haps(haps_list,shuffle=False):
     
     return haps_paired
 
-def recombine_haps(hap_pair,site_locs,recomb_rate=10**-8):
+def recombine_haps(hap_pair,site_locs,
+                   recomb_rate=10**-8,mutate_rate=10**-8):
     """
     Takes as input a pair of concrete haps giving the allele
     at each variable site as well as a list of positions for
@@ -52,18 +53,228 @@ def recombine_haps(hap_pair,site_locs,recomb_rate=10**-8):
     an exponential distribution
     """
     
-    scale = 1/recomb_rate
+    recomb_scale = 1/recomb_rate
+    mutate_scale = 1/mutate_rate
     
     assert len(hap_pair[0]) == len(hap_pair[1]), "Length of two haplotypes is different"
     assert len(hap_pair[0]) == len(site_locs), "Different length of hap and of list of site locations"
     
+    #Current location
     cur_loc = site_locs[0]
+    cur_loc_index = 0
+    
+    #Hap we are currently copying from
+    using_hap = random.choice([0,1])
+    
+    #List which will contain the pieces that make up the final hap
+    final_hap_list = []
+    
+    while cur_loc <= site_locs[-1]:
+        next_break_distance = np.random.exponential(recomb_scale)
+        
+        
+        new_loc = cur_loc+np.ceil(next_break_distance)
+            
+        new_loc_index = np.searchsorted(site_locs,new_loc)
+        
+        adding = hap_pair[using_hap][cur_loc_index:new_loc_index]
+        
+        final_hap_list.append(adding)
+        
+        #Switch around our copying haplotype
+        using_hap = 1-using_hap
+        
+        cur_loc = new_loc
+        cur_loc_index = new_loc_index
+    
+    return_value = np.concatenate(final_hap_list)
+    
+    #Add in mutations
+    mutation_points = []
+    
+    cur_loc = site_locs[0]
+    cur_loc_index = 0
+    
+    while cur_loc <= site_locs[-1]:
+        next_mutation_distance = np.random.exponential(mutate_scale)
+        
+        
+        new_loc = cur_loc+np.floor(next_mutation_distance)
+            
+        new_loc_index = np.searchsorted(site_locs,new_loc)
+        
+        if new_loc_index < len(site_locs):
+            mutation_points.append(new_loc_index)
+        
+        cur_loc = new_loc
+        cur_loc_index = new_loc_index
+    
+    base_vals = return_value[mutation_points]
+    
+    mutated_vals = 1-base_vals
+    
+    return_value[mutation_points] = mutated_vals
+    
+    return return_value
+
+def create_offspring(first_pair,second_pair,
+                     site_locs,recomb_rate=10**-8,
+                     mutate_rate=10**-8):
+    """
+    Create an offspring from the two parents which have haplotype pairs
+    first_pair and second_pair respectively by running recombine_haps on 
+    each of the pairs and then returning the combined result
+    """
+    
+    first_hap = recombine_haps(first_pair,site_locs,recomb_rate=recomb_rate)
+    second_hap = recombine_haps(second_pair,site_locs,recomb_rate=recomb_rate)
+    
+    return [first_hap,second_hap]
+
+def create_new_generation(parent_haps,site_locs,num_offspring,
+                          recomb_rate=10**-8,mutate_rate=10**-8):
+    """
+    Create a new generation of num_offspring offspring given a list
+    of pairs of parental haplotypes. Each offspring is made by randomly
+    choosing 
+    """
+    new_pairs = []
+    
+    for i in range(num_offspring):
+        parents = random.sample(parent_haps,2)
+        
+        offspring = create_offspring(parents[0],parents[1],
+                                     site_locs,recomb_rate=recomb_rate,
+                                     mutate_rate=mutate_rate)
+        new_pairs.append(offspring)
+    return new_pairs
+
+def get_reads_from_sample(hap_pair,read_depth,error_rate=0.02):
+    """
+    Simulates sequencing an individual made up of a pair of haplotypes 
+    up to read_depth average coverage. Returns an array of size len(hap_pair[0])*2
+    where the first column gives the number of 0 reads and the second column gives
+    the number of 1 reads
+    """
+    
+    num_sites = len(hap_pair[0])
+    
+    num_reads_at_site = np.random.poisson(lam=read_depth,size=num_sites)
+    
+    site_sum = hap_pair[0]+hap_pair[1]
+    
+    zeros = np.where(site_sum == 0)[0]
+    ones = np.where(site_sum == 1)[0]
+    
+    twos = np.where(site_sum == 2)[0]
+    
+    zero_read_counts = num_reads_at_site[zeros]
+    one_read_counts = num_reads_at_site[ones]
+    two_read_counts = num_reads_at_site[twos]
+    
+    zero_draws = np.random.binomial(zero_read_counts,error_rate)
+    one_draws = np.random.binomial(one_read_counts,0.5)
+    two_draws = np.random.binomial(two_read_counts,1-error_rate)
+    
+    zero_basics = zero_read_counts-zero_draws
+    one_basics = one_read_counts-one_draws
+    two_basics = two_read_counts-two_draws
+    
+    zero_concated = np.concatenate([np.reshape(zero_basics,(-1,1)),np.reshape(zero_draws,(-1,1))],axis=1)
+    one_concated = np.concatenate([np.reshape(one_basics,(-1,1)),np.reshape(one_draws,(-1,1))],axis=1)
+    two_concated = np.concatenate([np.reshape(two_basics,(-1,1)),np.reshape(two_draws,(-1,1))],axis=1)
+    
+    full_scaffold = np.zeros((num_sites,2))
+    
+    full_scaffold[zeros,:] = zero_concated
+    full_scaffold[ones,:] = one_concated
+    full_scaffold[twos,:] = two_concated
+    
+    return full_scaffold
+    
+def read_sample_all_individuals(individual_list,read_depth,error_rate=0.02):
+    """
+    Takes a list of pairs of haps (individuals) and samples them all to
+    read_depth read coverage on average
+    """
+    sampled = []
+    
+    for item in individual_list:
+        reads = get_reads_from_sample(item,read_depth,error_rate=error_rate)
+        sampled.append(reads)
+    
+    np_array = np.array(sampled,dtype=int)
+    
+    return np_array
+    
+def chunk_up_data(positions_list,reads_array,
+                  starting_pos,block_size,shift_size,
+                  error_rate=0.02,
+                  min_total_reads=5):
+    """
+    Breaks up the positions_list and reads_array (where elements of the positions_list 
+    correspond to the location of the site at that index in reads_array) into chunks
+    of size block_size where consecutive chunks differ by shift_size in their starting
+    location
+    
+    If we have fewer than min_total_reads or error_rate*num_samples (whichever is higher)
+    at a site then we mark it as a 0 to indicate it is low quality and not to be considered
+    for our founder inference algorithm
+    """
+    chunked_positions = []
+    chunked_reads = []
+    chunked_keep_flags = []
+    
+    num_samples = reads_array.shape[0]
+    
+    cur_pos = starting_pos
+    cur_index = 0
+    last_site = positions_list[-1]
+    
+    while cur_pos < last_site:
+        block_start_index = cur_index
+        
+        block_end_pos = cur_pos+block_size
+        
+        end_index = cur_index
+        while end_index < len(positions_list) and positions_list[end_index] < block_end_pos:
+            end_index += 1
+        
+        block_positions = positions_list[cur_index:end_index]
+        extract_indices = list(range(cur_index,end_index))
+        
+        block_reads_array = reads_array[:,extract_indices,:]
+        
+        total_read_pos = np.sum(block_reads_array,axis=(0,2))
+        
+        block_keep_flags = (total_read_pos >= max(min_total_reads,error_rate*num_samples)).astype(int)
+        
+        chunked_positions.append(block_positions)
+        chunked_reads.append(block_reads_array)
+        chunked_keep_flags.append(block_keep_flags)
+        
+        cur_pos = cur_pos + shift_size
+        
+        while cur_index < len(positions_list) and positions_list[cur_index] < cur_pos:
+            cur_index += 1
+    
+    return (chunked_positions[:-1],chunked_keep_flags[:-1],chunked_reads[:-1])
+        
+        
 #%%
-data = sa[1]
+data = final_test[1]
     
 cm = concretify_haps(data)
 pa = pairup_haps(cm)
-
-print(pa)
 #%%
-print(np.random.exponential(10**8))
+f1 = create_new_generation(pa,final_test[0],10,recomb_rate=10**-6,mutate_rate=10**-6)
+f2 = create_new_generation(f1,final_test[0],100,recomb_rate=10**-6,mutate_rate=10**-6)
+f3 = create_new_generation(f2,final_test[0],200,recomb_rate=10**-6,mutate_rate=10**-6)
+
+all_offspring = [xs for x in [f1,f2,f3] for xs in x]
+#%%
+new_reads_array = read_sample_all_individuals(all_offspring,3)
+#%%
+(simd_pos,simd_keep_flags,simd_reads) = chunk_up_data(final_test[0],new_reads_array,2500000,100000,50000)
+#%%
+simd_haps = generate_long_haplotypes(simd_pos,simd_reads,6,simd_keep_flags)
